a03552259f1648d3bd8b8e3e60c3e877146fedea
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-05-04 19:05:32 +0200

    Mise à jour de l'API FLASK

---
+++ api/p7/test1.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+def test_api(url):
+    # Données à prédire
+    data = {
+        'message': 'the film is good'
+    }
+
+    # Convertir les données en JSON
+    #data_json = json.dumps(data)
+
+    # Envoyer la requête POST à l'API Flask
+    response = requests.post(url, data=data)
+
+    # Afficher la réponse de l'API Flask
+    #prediction = response.json()[0]
+
+    # Afficher la réponse de l'API Flask
+    print(response.text)
+    print(response.status_code)
+    
+    
+    
+test_api("http://127.0.0.1:3001/predict_post")
